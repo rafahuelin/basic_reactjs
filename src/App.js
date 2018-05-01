350db03c761c0811a13aab8dac79eb6705432ab6
Rafa <rafahuelin@gmail.com>
2018-05-01 16:41:34 +0200

    display todos in cards and tasks counter in the navbar

--- src/App.js
+++ src/App.js
@@ -8,19 +8,48 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            title: "Tasks App",
-            amount: "10"
+            todos
         }
     }
 
     render() {
+        const todos = this.state.todos.map((todo, i) => {
+            return(
+                <div className="col-md-4">
+                    <div className="card mt-4">
+                        <div className="card-header">
+                            <h3>{ todo.title }</h3>
+                            <span className="badge badge-pill badge-danger ml-2">
+                                { todo.priority }
+                            </span>
+                        </div>
+                        <div className="card-body">
+                            <p>{ todo.description }</p>
+                            <p><mark>{ todo.responsible }</mark></p>
+                        </div>
+
+                    </div>
+                </div>
+            )
+        });
+
         return (
             <div className="App">
                 <nav className="navbar navbar-dark bg-dark">
                     <a href="#" className="text-white">
-                        { this.state.title } &mdash; { this.state.amount }
+                        Tasks
+                        <span className="badge badge-pill badge-light ml-2">
+                            { this.state.todos.length }
+                        </span>
                     </a>
                 </nav>
+
+                <div className="container">
+                    <div className="row mt-4">
+                        {todos}
+                    </div>
+                </div>
+
                 <img src={logo} className="App-logo" alt="logo"/>
             </div>
         );
