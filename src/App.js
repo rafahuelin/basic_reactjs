91f994be33f2a0c8e289272a34b5ffafd6ae93e8
Rafa <rafahuelin@gmail.com>
2018-05-01 15:53:58 +0200

    add json file to simulate data from backend

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Navigation from './components/Navigation';
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <Navigation title="First navbar" />
-          <Navigation title="Second navbar" />
-          <Navigation title="Third navbar" />
+          <nav className="navbar navbar-dark bg-dark">
+              <a href="#" className="text-white">
+                  Navbar
+              </a>
+          </nav>
           <img src={logo} className="App-logo" alt="logo" />
       </div>
     );
