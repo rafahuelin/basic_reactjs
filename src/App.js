acde1cc290898c0bfde4de579e2c9224b7e6116c
Rafa <rafahuelin@gmail.com>
2018-05-01 16:12:11 +0200

    set state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {todos} from "./todos";
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <nav className="navbar navbar-dark bg-dark">
-              <a href="#" className="text-white">
-                  Navbar
-              </a>
-          </nav>
-          <img src={logo} className="App-logo" alt="logo" />
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            title: "Tasks App",
+            amount: "10"
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <nav className="navbar navbar-dark bg-dark">
+                    <a href="#" className="text-white">
+                        { this.state.title } &mdash; { this.state.amount }
+                    </a>
+                </nav>
+                <img src={logo} className="App-logo" alt="logo"/>
+            </div>
+        );
+    }
 }
 
 export default App;
